dc1c2501604c3fd27c50958902912bee993ac6b9
Daniel Chávez <danielsaul96@gmail.com>
2020-08-25 01:30:20 -0500

    extended help message

--- README.md
+++ README.md
@@ -2,6 +2,5 @@
 A utility to find duplicate files in a filesystem
 
 # TODO
-* Better help instructions.
 * Directory exclusion and recursion limits.
 * Tests.
--- main.go
+++ main.go
@@ -30,9 +30,50 @@ func main() {
 		av := AvaiableHashCreators(avaiableHashCreators)
 
 		fmt.Fprintf(flag.CommandLine.Output(), "usage: %s [FLAGS] [ROOT_DIR]\n\n", filepath.Base(os.Args[0]))
-		fmt.Fprintf(flag.CommandLine.Output(), "Recursively search a directory for duplicate files.\n\n")
-		fmt.Fprintf(flag.CommandLine.Output(), "Avaiable hash functions: %s.\n\n", strings.Join(av.Names(), ", "))
+		fmt.Fprintf(flag.CommandLine.Output(), "Recursively search a directory and subdirectories for duplicate files.\n\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "Flags:\n\n")
 		flag.PrintDefaults()
+		fmt.Fprintf(flag.CommandLine.Output(), "\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "Avaiable hash functions: %s.\n\n", strings.Join(av.Names(), ", "))
+		fmt.Fprintf(flag.CommandLine.Output(), `The general algorithm is as follows: if two files have different sizes,
+they're inmediately considered different. If they have the same size,
+they're hashed (only calculated once per file, when needed) and their
+sums are compared for a final veredict.
+		
+`)
+		fmt.Fprintf(flag.CommandLine.Output(), `The final report has the following format:
+
+HEX_HASH_SUM
+	CLONED_FILE_1
+	CLONED_FILE_2
+	...
+
+And so on, for every group of files that were found to be clones. Results
+are sorted so, in theory, multiple runs in the same directory should
+produce the same output, if the files don't change.
+
+`)
+		fmt.Fprintf(flag.CommandLine.Output(), `Remember not to take the results as gospel! No byte-for-byte comparison is
+attempted to absolutely confirm two collisions are equal. False positves
+with a good hash are unlikely, but not impossible. If the risk is not
+acceptable, don't do something like passing the output directly to a
+deleting script without byte-for-byte or manual double checking.
+
+`)
+		fmt.Fprintf(flag.CommandLine.Output(), `Obviously, the more files are compared, the more memory is required for
+the internal structure, even if only the strictly necessary is saved
+(names, sizes, and cached hashes). Also, the hash function is necessarily
+a compromise between hashing speed, size when cached, and probability
+of collision. The default (SHA-1) was chosen for being relatively
+speedy in modern machines and having an almost negligible chance of
+collision in practice (this application is *not* meant for security
+hardiness). Of course, some other algorithms are provided, but keep in
+mind: the bottleneck will probably be just the tree traversing and all
+those stat calls. In any case, hashing should only come into play in the
+case of many files with the exact same size, which should be, hopefully,
+uncommon in most practical cases.
+
+`)
 	}
 
 	flag.Parse()
